e9cb2aa23025e32913e8d4936da0d763038fd507
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-10-31 07:26:35 +0000

    basic setup and deploy to vercel

---
+++ README.md
@@ -0,0 +1,7 @@
+# Van Life App
+
+
+## Todos
+- [x] Basic setup
+- [] create a git repo and Deploy to vercel
+- [] 
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white text-center p-4">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
